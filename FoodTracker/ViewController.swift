0407e20cd997aab8c0fa118354f0921b39f96a42
Christine W <cjwang9999@gmail.com>
2017-11-11 13:55:44 -0500

    firstCommit

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,19 +7,42 @@
 //
 
 import UIKit
+//extends UIViewController
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    //MARK: Properties
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
+    }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        //MARK: hide the keyboard
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField){
+        mealNameLabel.text = textField.text
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+    @IBAction func selectImageFromImageLibrary(_ sender: UITapGestureRecognizer) {
+        //Hides the Keyboard
+        nameTextField.resignFirstResponder()
+        //UIImagePickerController is a control that allows users pick media from their photo library
+        let imagePickerController = UIImagePickerController()
+        
+        //Only allows photos to be picked, not taken
+        imagePickerController.sourceType = .photoLibrary
     }
 
-
+    @IBAction func setDefaultLabelButton(_ sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
